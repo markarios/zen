dd7dcd64d39063a2f9bfd98aa9b7b8b79329c4e5
markarios <mark.rios@cal.berkeley.edu>
2015-01-10 17:05:58 -0800

    added a wish list

--- d3.zen.js
+++ d3.zen.js
@@ -41,11 +41,20 @@ d3.zen.forceChart = function module(){
 				}
 			}
 			svg.attr({ width: width, height: height})
-
+			// add linkStrength(0.1), between [0,1], can be a function
+			// add friction(.9), 1 frictionless, 0 freeses all particles, value outside of [0,1] are not recommended
+			// add gravity(.1), can be zero, but if zero bound nodes from escaping the layout
+			// add theta(0.8), determines the accuracy of the Barnes-Hut approximation
+			// add alpha(0.1)
+			// add chargeDistance (max distance at which force is applied, default Infinity, a finite number improves performance)
+			// linkDistance, can be a function
+			// charge, can be a function (negative: respulsion, postive:attraction)
+			// nodes can be symbols are images
+			// edges can be any dom element as well, like nodes
 			svg.select("\." + containerClass).attr("transform","translate(" + margin.left + "," + margin.top + " )");
 			var force = d3.layout.force()
 				.nodes(_data.nodes)
-				.links(_data.edges)
+				.links(_data.edges) 
 				.size([ chartW, chartH])
 				.linkDistance(linkDistance)
 				.charge(charge)
